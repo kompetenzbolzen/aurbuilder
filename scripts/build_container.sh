8f432e1b1d42d1a45597c32256af9b611b422000
Jonas Gunz <himself@jonasgunz.de>
2023-07-24 14:38:43 +0200

    move container files to subfolder, add nginx conf

--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PODMAN=$(which podman)
-DIR=${DIR:-/usr/share/aurbuilder}
+DIR=${DIR:-/usr/share/aurbuilder/container}
 
 $PODMAN pull archlinux:base-devel
 $PODMAN build --no-cache -t aurbuilder "$DIR"
